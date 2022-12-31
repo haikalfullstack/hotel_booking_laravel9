c7ca96a5f4a5a891b0e5aaaa1cbea89ff62eb2d4
ekalrozhan <ekalrozhan@gmail.com>
2023-01-01 00:59:31 +0800

    login and forget password page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 
 
@@ -13,3 +14,5 @@ Route::get('/', function () {
 
 // Admin
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/login', [AdminloginController::class, 'index'])->name('admin_login');
+Route::get('/admin/forget-password', [AdminloginController::class, 'forget_password'])->name('admin_forget_password');
