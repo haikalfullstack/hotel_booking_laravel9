fce988549537af4982049667816398c305ebc672
ekalrozhan <ekalrozhan@gmail.com>
2023-01-02 15:15:31 +0800

    Admin profile information and photo update

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -21,7 +21,7 @@
     <div id="app">
         <div class="main-wrapper">
 
-            
+
             @include('admin.layout.nav')
 
 
@@ -45,6 +45,39 @@
     </div>
 
     @include('admin.layout.scripts_footer')
+
+
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <script>
+                iziToast.error({
+                    title: '',
+                    position: 'topRight',
+                    message: '{{ $error }}',
+                });
+            </script>
+        @endforeach
+    @endif
+
+    @if (session()->get('error'))
+        <script>
+            iziToast.error({
+                title: '',
+                position: 'topRight',
+                message: '{{ session()->get('error') }}',
+            });
+        </script>
+    @endif
+
+    @if (session()->get('success'))
+        <script>
+            iziToast.success({
+                title: '',
+                position: 'topRight',
+                message: '{{ session()->get('success') }}',
+            });
+        </script>
+    @endif
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminProfileController;
 
 
 
@@ -14,10 +15,21 @@ Route::get('/', function () {
 
 // Admin 
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
+
 Route::get('/admin/login', [AdminloginController::class, 'index'])->name('admin_login');
+
 Route::post('/admin/login-submit', [AdminloginController::class, 'login_submit'])->name('admin_login_submit');
+
 Route::get('/admin/forget-password', [AdminloginController::class, 'forget_password'])->name('admin_forget_password');
+
 Route::get('/admin/logout', [AdminloginController::class, 'logout'])->name('admin_logout');
+
 Route::post('/admin/forget-password-submit', [AdminloginController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
+
 Route::get('/admin/reset-password/{token}/{email}', [AdminloginController::class, 'reset_password'])->name('admin_reset_password');
+
 Route::post('/admin/reset-password-submit', [AdminloginController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
+
+Route::get('/admin/edit-profile', [AdminProfileController::class, 'index'])->name('admin_profile')->middleware('admin:admin');
+
+Route::post('/admin/edit-profile-submit', [AdminProfileController::class, 'profile_submit'])->name('admin_profile_submit');
